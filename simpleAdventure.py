59fb4a576297bdd723ee19b96a957b9b17c67459
Rasmus Rasmussen <rasras@gmail.com>
2017-09-18 10:11:36 -0700

    Now Python 3 compliant
    Changed a bit of outdated syntax, so the script works with current versions of Python. Tested on 3.6.

--- simpleAdventure.py
+++ simpleAdventure.py
@@ -1,19 +1,18 @@
-# simpleAdventure v. 0.1b
-# officially lives here -- https://github.com/theprint/simpleAdventure
+# simpleAdventure v. 0.2b
+# Officially lives here -- https://github.com/theprint/simpleAdventure
 # simpleAdventure is really just an exercise in Python, written in 2 days after studying the language for about 2 weeks. The game is a potentially endless text based adventure that allows a character to level up and includes an element of exploration.
 # If I had had more time, I would have included a storyline and an ongoing quest to collect items (there is already an inventory included with the player object, I just didn't get around to using it.
 # Feel free to add to this code, or modify it in any way you see fit. Thanks, Rasmus - twitter.com/theprint / rasmusrasmussen.com
 import random
 import math
-import time
 
 # This is the player data
 player = {
-  "name" : "Jack",
+	"name" : "Jack",
 	"health" : 10,
 	"maxHealth" : 10,
 	"gold" : 100,
-	"inventory" : ["shoes", "blanket","healing potion"],
+	"inventory" : ["shoes", "blanket"],
 	"level" : 1,
 	"xp" : 0,
 	"xpForNextLevel" : 100,
@@ -25,7 +24,7 @@ player = {
 
 # Initialize gamemap and tile types.
 gameMap = {}
-tileType = ["Town","Plains","Hills","Mountains","Forest","Swamp","Badlands","Dungeon","River","Tundra"]
+tileType = ["Town","Plains","Hills","Mountains","Forest","Swamp","Badlands","Dungeon"]
 
 # Generates a tile on the game map.
 def makeTile(id,x,y):
@@ -56,21 +55,21 @@ def makeMap(rows,columns):
 	
 # generate a player.		
 def makePlayer():
-	player["name"] = raw_input("Give your character a name: ")
-	print "\nYour character is named %s. Your health is full (%s hp), and you currently hold %s gold pieces. You inventory is empty." % (player["name"], player["health"],player["gold"])
-	print "\t%s is a 1st level character, who needs %s xp to gain a level. Doing so will lead to more health. You get xp from exploring and overcoming encounters." % (player["name"],player["xpForNextLevel"])
-	print "\tYour adventure begins in your home town of Slumberville. Time to go explore the lands..."
+	player["name"] = input("Give your character a name: ")
+	print ("\nYour character is named {0}. Your health is full ({1} hp), and you currently hold {2} gold pieces. You inventory is empty.".format(player["name"], player["health"],player["gold"]))
+	print ("\t%s is a 1st level character, who needs %s xp to gain a level. Doing so will lead to more health. You get xp from exploring and overcoming encounters." % (player["name"],player["xpForNextLevel"]))
+	print ("\tYour adventure begins in your home town of Slumberville. Time to go explore the lands...")
 
 def hitEnter():
-	raw_input("\nHit Enter to continue.")
+	input("\nHit Enter to continue.")
 	
 # Load tile description
 def description():
 	isExplored = "explored"
 	if gameMap[player["locationID"]]["explored"] == False:
 		isExplored = "unexplored"
-	print "\nCurrent location: " + gameMap[player["locationID"]]["type"]
-	print "(X: " + str(player["locationX"]) + ", Y: " + str(player["locationY"]) + ") This area is " + isExplored + ". TILE# " + str(player["locationID"])
+	print ("\nCurrent location: " + gameMap[player["locationID"]]["type"])
+	print ("(X: " + str(player["locationX"]) + ", Y: " + str(player["locationY"]) + ") This area is " + isExplored + ". TILE# " + str(player["locationID"]))
 
 # move around the map.
 def movePlayer(direction):
@@ -111,126 +110,49 @@ def movePlayer(direction):
 # adds xp and levels up the player as necessary.
 def awardXP(amount):
 	player["xp"] += amount
-	print "\n%s earned %s experience points." % (player["name"],amount)
+	print ("\n%s earned %s experience points." % (player["name"],amount))
 	if player["xp"] >= player["xpForNextLevel"]:
 		player["level"] += 1
 		player["maxHealth"] += 5
 		player["health"] = player["maxHealth"]
 		player["xpForNextLevel"] += int(math.floor(player["xpForNextLevel"]*1.55))
-		print "LEVEL UP -- %s is now level %s!" % (player["name"], player["level"])
-
-# Heals the player to max hit points.		
+		print ("LEVEL UP -- %s is now level %s!" % (player["name"], player["level"]))
+		
 def healPC():
-	print "You have healed %s hit points." % (player["maxHealth"]-player["health"])
+	print ("You have healed %s hit points." % (player["maxHealth"]-player["health"]))
 	player["health"] = player["maxHealth"]
-
-# Pick a monster type!
-def monsterType():
-	monsterList = ["giant spider","goblin","ghost","zombie","ogre","kobold","orc","giant rat","giant beetle","minotaur","dragon","hill giant","wolf","bandit","bear","sorceror"]
-	monster = random.choice(monsterList)
-	return monster
-
-# a monster tries hitting the player
-def monsterAttack(monsterDamage, monsterType):
-		print "The %s hits back at you..." % (monsterType)
-		time.sleep(0.5)
-		monsterToHit = random.randint(1,10)
-		if monsterToHit >= 6:
-			print "It hits you for %s points of damage." % (monsterDamage)
-			player["health"] -= monsterDamage
-		else:
-			print "The blow misses you."
-		time.sleep(0.5)
-
-# Combat control loop.
-def combat(level):
-	fighting = True
-	theMonster = monsterType() #returns a monster type.
 	
-	# adjust level for specific monster types.
-	if theMonster == "ogre" or theMonster == "minotaur" or theMonster == "hill giant" or theMonster == "sorceror":
-		level += player["level"]
-	elif theMonster == "dragon":
-		level += int(player["level"]*1.8)
-		
-	# Initialize monster stats.
-	monsterHP = random.randint(3,level*3) + level
-	monsterDamage = random.randint(level,int(level*2.75))
-	theXPReward = int((monsterHP + monsterDamage) * (level*1.5))
+def doFight():
+	monsterType = ["Goblin","Orc","Bandit","Zombie","Skeleton","Slime","Kobold","Giant Bat","Giant Beetle","Wolf","Bear"]
+	myRace = random.choice(monsterType)
+	theMonster = {
+		"race" : myRace,
+		"hit points" : random.randrange(2,9),
+		"treasure" : random.randrange(0,20)
+	}
+	print ("\n1 %s appears before you." % theMonster["race"])
 	
-	if monsterHP > (player["health"]-2):
-		description = "a tough looking"
-	elif monsterDamage > (player["health"]/2):
-		description = "a strong looking"
-	elif monsterHP <= (player["health"]/2):
-		description = "a weak looking"
-	else:
-		description = "an angry looking"
-	print "\nBefore you stands " + description + " " + theMonster + "."
-	while fighting == True and player["health"] > 0:
-		print "a = attack"
-		print "f = flee"
-		if "healing potion" in player["inventory"]:
-			print "h = use healing potion"
-		playerAction = raw_input("Pick an action: ").lower()
-		if playerAction == "a":
-			#attack monster
-			toHit = random.randint(1,10) + level
-			time.sleep(0.5)
-			if toHit >= monsterHP:
-				theDamage = random.randint(2,5)
-				monsterHP -= theDamage
-				print "\nYou hit the %s for %s hit points." % (theMonster, theDamage)
-				time.sleep(0.5)
-				if monsterHP <= 0:
-					print "Your opponent falls lifeless to the ground."
-					awardXP(theXPReward)
-					fighting = False
-				else:
-					monsterAttack(monsterDamage,theMonster)
-			else:
-				print "Your blow misses its target."
-				monsterAttack(monsterDamage,theMonster)			
-		elif playerAction == "f":
-			#try to escape
-			print "You try to get away from the %s..." % (theMonster)
-			time.sleep(1)
-			escape = random.randint(1,5)
-			if escape > 3:
-				print "You escape and live to fight another day."
-				fighting = False
-			else:
-				print "You don't get away this time! Your opponent hits you for %s points of damage." % (int(monsterDamage/2))
-				player["health"] -= int(monsterDamage/2)
-		elif playerAction == "h" and "healing potion" in player["inventory"]:
-			print "You quaff a healing potion! You health has been restored."
-			player["health"] = player["maxHealth"]
-			player["inventory"].remove("healing potion")
-		else:
-			"I don't understand that command."
-		
-# lets the player perform a standard action	
 def getAction():
-	print "\nAvailable actions:"
+	print ("\nAvailable actions:")
 	actionString = ""
 	if gameMap[player["locationID"]]["explored"] == False:
 		actionString += "\nx = explore the area."
 	if player["locationID"] == 13 and player["health"] < player["maxHealth"]:
 		actionString += "\nr = rest and recover."
-	print actionString 
-	print "n = north / s = south / w = go west / e = go east."
-	print "i = view inventory and gold."
-	print "c = character sheet."
+	print (actionString )
+	print ("n = north / s = south / w = go west / e = go east.")
+	print ("i = view inventory and gold.")
+	print ("c = character sheet.")
 	# get a command from the player:
-	choice = raw_input("Pick an action and hit Enter: ").lower()
+	choice = input("Pick an action and hit Enter: ").lower()
 	
 	#out put result of command...
-	print "\n"
+	print ("\n")
 	
 	#display inventory
 	if choice == "i":
-		print "You are currently carrying: " + "%s" % ", ".join(map(str, player["inventory"]))
-		print "Gold: " + str(player["gold"])
+		print ("You are currently carrying: " + "%s" % ", ".join(map(str, player["inventory"])))
+		print ("Gold: " + str(player["gold"]))
 	# Moving along
 	elif choice == "n" or choice == "e" or choice == "w" or choice == "s":
 		movePlayer(choice)
@@ -238,57 +160,37 @@ def getAction():
 	elif choice == "x":
 		if gameMap[player["locationID"]]["explored"] == False:
 			player["exploredTiles"] += 1
-			sumNum = random.randrange(1,10)
-			#random treasure!
+			sumNum = random.randrange(1,3)
 			if sumNum == 1:
-				print "\nYou find gold!"
-				gold = random.randrange(5,26)
-				print str(gold) + " added to your holdings..."
+				print ("\nYou find gold!")
+				gold = random.randrange(10,60)
+				print (str(gold) + " added to your holdings...")
 				player["gold"] += gold
-				if gold > 12:
-					awardXP(int(math.floor(gold/12)))
-			# here be monsters!
-			elif sumNum >= 2 and sumNum <= 6:
-				print "Oh no! Monsters! A fight ensues..."
-				time.sleep(1)
-				theLevel = player["level"]
-				# This makes fighting in dungeons way dangerous, while badlands, mountains and tundra is slightly tougher.
-				if gameMap[player["locationID"]]["type"] == "Dungeon":
-					theLevel += 2
-				elif gameMap[player["locationID"]]["type"] == "Badlands" or gameMap[player["locationID"]]["type"] == "Mountain" or gameMap[player["locationID"]]["type"] == "Tundra":
-					theLevel += 1
-				# start the fight!
-				combat(theLevel)
-			# Here be other encounters
+				awardXP(int(math.floor(gold/12)))
+			elif sumNum == 2:
+				print ("Oh no! Monsters!")
+				damage = random.randrange(1,4)
+				print ("You take " + str(damage) + " points of damage.")
+				player["health"] -= damage
+				if player["health"] > 0:
+					awardXP(damage*5)
 			else:
-				if gameMap[player["locationID"]]["type"] == "Town" and player["gold"] > (player["level"] * 15):
-					print "You find a shopkeeper willing to sell you a healing potion!"
-					price = player["level"]*15
-					buy = raw_input("Do you wish to buy it for %s gold?" % (price)).lower()
-					if buy == "y":
-						player["gold"] -= price
-						player["inventory"].append("healing potion")
-						print "A healing potion has been added to your inventory."
-					elif buy == "n":
-						print "The shopkeeper shrugs and puts away his potion."
-					else:
-						print "The merchant shakes his head. 'I don't understand.'"
-				else:
-					print "You look around, but find nothing of interest..."
+				print ("You look around, but find nothing of interest...")
+				awardXP(1)
 			
 			gameMap[player["locationID"]]["explored"] = True
 		else:
-			print "There is nothing to explore here."
+			print ("There is nothing to explore here.")
 	# rest and recover
 	elif choice == "r" and player["locationID"] == 13 and player["health"] < player["maxHealth"]:
 		healPC()
 	# display character sheet
 	elif choice == "c":
-		print player["name"] + " / Level: " + str(player["level"]) + " XP: " + str(player["xp"]) + " (" + str(player["xpForNextLevel"]) + " needed)"
-		print "Hit points: " + str(player["health"]) + " (max: " + str(player["maxHealth"]) + ")"
+		print (player["name"] + " / Level: " + str(player["level"]) + " XP: " + str(player["xp"]) + " (" + str(player["xpForNextLevel"]) + " needed)")
+		print ("Hit points: " + str(player["health"]) + " (max: " + str(player["maxHealth"]) + ")")
 	
 	else:
-		print "I don't know that command."
+		print ("I don't know that command.")
 # This is where the game action happens...
 
 # Generate the world...
@@ -305,7 +207,7 @@ hitEnter()
 while still_alive:
 	getAction()
 	if player["exploredTiles"] == 25: #map is fully explored, a new one is generated...
-		print "You have explored the entire map!\nA friendly wizard teleports you to a new one..."
+		print ("You have explored the entire map!\nA friendly wizard teleports you to a new one...")
 		#this basically resets the game, except for the player who is only moved to the new "home" tile.
 		hitEnter()
 		player["locationID"] = 13
@@ -316,6 +218,6 @@ while still_alive:
 		player["exploredTiles"] = 1
 		description()
 	if player["health"] <= 0: # the player died
-		print "\nYou have died."
+		print ("\nYou have died.")
 		still_alive = False
 		#the game ends.
